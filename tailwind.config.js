6f54b501fdbc9088f934394de36c4d7b14a434a6
kane1213 <kane711213@gmail>
2023-10-30 23:19:43 +0800

    add tailwind success

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
+    
   },
   plugins: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
 }
-
