3826334f8d8f293de7eb2dee08c67da236f16bfb
kane1213 <kane711213@gmail.com>
2023-10-31 17:11:56 +0800

    establish mpa

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import { HashRouter } from 'react-router-dom'
-
-import { Suspense } from 'react'
-import AppRoutes from './router'
-import Header from '@/components/Header'
 
 function App() {
   
-  return <HashRouter>
-    <Header />
-    <div className="main-work">
-      <Suspense fallback={<div className="text-center mt-[48vh] text-black">Loading...</div>}>
-        <AppRoutes />
-      </Suspense>
-    </div>
-  </HashRouter>
+  return <div>
+    2234
+  </div>
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import mpa from 'vite-plugin-mpa'
+
 import path from 'path'
+
+// console.log(mpa)
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactRefresh(), mpa.default()],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src'),
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: './'
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'src/pages/Home/index.tsx',
+        // about: 'src/pages/About/index.tsx',
+        // contact: 'src/pages/Contact/index.tsx',
+      },
+    },
+  },
 })
